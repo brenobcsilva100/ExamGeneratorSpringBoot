90a24513303800fb16719239fb25f3aab743bf65
Breno <brenobcsilva@gmail.com>
2023-04-13 16:22:20 -0300

    Configurado seguranca. Agora comecamos com frontend em angular

--- src/main/java/br/com/devdojo/examgenerator/security/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/examgenerator/security/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package br.com.devdojo.examgenerator.security.config;
 
+import br.com.devdojo.examgenerator.security.filter.JwtAuthenticationFilter;
+import br.com.devdojo.examgenerator.security.filter.JwtAuthorizationFilter;
 import br.com.devdojo.examgenerator.security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,11 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.cors().configurationSource(httpServletRequest ->  new CorsConfiguration()
+                .applyPermitDefaultValues())
+                .and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/*/professor/**").hasRole("PROFESSOR")
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), customUserDetailsService));
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
